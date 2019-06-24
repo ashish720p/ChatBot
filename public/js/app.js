ce01327ec8d23b1f0efc04e84a17ecadcfe9c60b
ashish720p <36568108+ashish720p@users.noreply.github.com>
2019-06-24 22:26:07 +0530

    Add files via upload

--- public/js/app.js
+++ public/js/app.js
@@ -1,35 +1,28 @@
 'use strict';
 
-$(function(){
 
-});
 
 const socket = io();
 const query = document.querySelector('#comment');
 const html = document.querySelector('#conversation');
 
-
-
-
-
-
 document.querySelector('#chat-send').addEventListener('click', () => {
 	var date = new Date();
-	var htmlResponse	=	"<div class=\"row message-body\">\
-	<div class=\"col-sm-12 message-main-sender\">\
-	<div class=\"sender\">\
-	<div class=\"message-text\">" +
-	query.value +
-	"</div>\
-	<span class=\"message-time pull-left\">"
-	+ date.getHours() + ":" + date.getMinutes() +
-	"</span>\
-	</div>\
-	</div>\
-	</div>";
-	query.value = '';
-	console.log(query.value);
-	html.innerHTML = html.innerHTML + htmlResponse;
+    	var htmlResponse	=	"<div class=\"row message-body\">\
+    	<div class=\"col-sm-12 message-main-sender\">\
+    	<div class=\"sender\">\
+    	<div class=\"message-text\">" +
+    	query.value +
+    	"</div><span class=\"message-time pull-left\">"
+    	+ date.getHours() + ":" + date.getMinutes() +
+    	"</span>\
+    	</div>\
+    	</div>\
+    	</div>";
+    	html.innerHTML = html.innerHTML + htmlResponse;
+    	socket.emit('chat request', query.value);
+    	query.value = '';
+    	console.log(query.value);
 	
 });
 
